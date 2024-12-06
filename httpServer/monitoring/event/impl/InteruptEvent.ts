5277fc41893eac9c7d50413921a49ea723d93169
Lee Min Waan <100744636+plsgivemeachane@users.noreply.github.com>
2024-12-06 22:27:03 +0700

    errrrm lol!

--- httpServer/monitoring/event/impl/InteruptEvent.ts
+++ httpServer/monitoring/event/impl/InteruptEvent.ts
@@ -5,6 +5,15 @@ import { AbstractRequestHandler } from "../handler/AbstractRequestHandler";
 
 
 export default class InteruptEvent extends Event<AbstractRequestHandler> {
+    getRequestId(event: RequestEvent): string {
+        throw new Error("Method not implemented.");
+    }
+    getOrCreateHandler(requestId: string): AbstractRequestHandler {
+        throw new Error("Method not implemented.");
+    }
+    cleanupHandler(requestId: string): void {
+        throw new Error("Method not implemented.");
+    }
     name: string = 'request:interupt';
     timestamp: number = Date.now();
 
